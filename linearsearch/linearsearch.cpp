8be283fdfff685d8202264c43bc7f40a3e8210f3
sabilafiridas <sabilafirdias@gmail.com>
2024-04-04 10:39:47 +0700

    Add project files.

---
+++ linearsearch/linearsearch.cpp
@@ -0,0 +1,13 @@
+// linearsearch.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int arr[20];
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
