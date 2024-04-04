07c74d385ef5ffe7e00fb96486a32ca1581256ce
sabilafiridas <sabilafirdias@gmail.com>
2024-04-04 10:46:42 +0700

    membuat output ke layar

--- linearsearch/linearsearch.cpp
+++ linearsearch/linearsearch.cpp
@@ -17,6 +17,10 @@ void input() {
         else
             cout << "\nArray should have minimum 1 and maximum 20 elements.\n\n";
     }
+
+    cout << "\n--------------------\n";
+    cout << " Enter array elements \n";
+    cout << "----------------------\n";
 }
 
 int main()
