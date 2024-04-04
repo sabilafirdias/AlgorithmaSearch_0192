159296e613156f0a9715ba7f46d46bd8f486bb46
sabilafiridas <sabilafirdias@gmail.com>
2024-04-04 11:15:04 +0700

    membuat prosedur display

--- linearsearch/linearsearch.cpp
+++ linearsearch/linearsearch.cpp
@@ -57,6 +57,13 @@ void LinearSearch() {
     while ((ch == 'y') || (ch == 'Y'));
 }
 
+void display() {
+    cout << "Array elements are: ";
+    for (i = 0; i < n; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+}
+
 int main()
 {
     
